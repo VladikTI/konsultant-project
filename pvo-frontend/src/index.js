072c19b17880c1b33b3e0338b7cab7808348a324
PochkaChaiki <pochka.chaiki@gmail.com>
2023-10-18 17:57:30 +0300

    Вернул фронтенд к рабочему состоянию

--- pvo-frontend/src/index.js
+++ pvo-frontend/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 import Login from "./routes/login";
-import Apply from "./routes/apply";
 import ErrorPage from "./error-page";
 import Admin from "./routes/admin";
 import Editing from "./routes/editing"
@@ -27,30 +23,10 @@ const router = createBrowserRouter([
     {
         path: "/editing",
         element: <Editing/>,
-
     },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const appTheme = createTheme({
-    palette: {
-        blue: {
-            main: '#00A3FF',
-            light: '#0072E5',
-            dark: '#0077FF',
-            contrastText: '#000000',
-        },
-    },
-    typography: {
-        fontFamily: "'Segoe UI', sans-serif;",
-    },
-});
-
-
 root.render(
-    <ThemeProvider theme={appTheme}>
-        <RouterProvider router={router}/>
-    </ThemeProvider>
+    <RouterProvider router={router}/>
 );
-
